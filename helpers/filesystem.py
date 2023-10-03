b724929a7f00e0c39ebab6d379be832e2eef6414
cristhianCEM <cristianbcer27@gmail.com>
2023-10-03 01:45:05 -0600

    feat: new function get_filename_from_url

--- helpers/filesystem.py
+++ helpers/filesystem.py
@@ -5,7 +5,7 @@ from string import ascii_letters, digits
 
 # Función para obtener y formatear el nombre de la carpeta de la URL
 # o generar uno aleatorio si es invalido o Nulo
-def get_folder_name_from_url(folder_url):
+def get_foldername_from_url(folder_url):
     parsed_url = urlparse(folder_url)
     path_parts = parsed_url.path.strip("/").split("/")
     folder_name = path_parts[-1] if path_parts else None
@@ -14,3 +14,11 @@ def get_folder_name_from_url(folder_url):
         random_str = ''.join(random_choices(ascii_letters + digits, k=10))
         formatted_folder_name = f"folder_{random_str}"
     return formatted_folder_name
+
+# Función para obtener el nombre del archivo de la URL
+# estilo  https://www.fireload.com/68ac0cd3a5f200b6/OP-503.zip?pt=WVZOTlN6azNWV1pCZUhkUFNVTTBkMjlqTW5oNVp6MDlPcDlSZnVDdnlGUUJrc2JkVG9WTUtuZz0%3D
+def get_filename_from_url(file_url):
+    parsed_url = urlparse(file_url)
+    path_parts = parsed_url.path.strip("/").split("/")
+    file_name = path_parts[-1] if path_parts else None
+    return file_name
