9c1c57492798d3ad992afbad9d46fe4c4e4c178e
cristhianCEM <cristianbcer27@gmail.com>
2023-10-03 15:14:32 -0600

    fix: various
    Move functions to fireload helpers

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,4 +1,3 @@
 TABLE_ID = "fileData"
-DOWNLOAD_BUTTON_ID = "downloadButton"
 DOWNLOAD_LINK_ID = "downloadButton"
 MAX_THREADS = 3
--- helpers/fireload.py
+++ helpers/fireload.py
@@ -1,6 +1,11 @@
+from helpers.constants import TABLE_ID, DOWNLOAD_LINK_ID, MAX_THREADS
+from helpers.filesystem import get_filename_from_url, exists_file
 from bs4 import BeautifulSoup
 import requests
-from helpers.constants import TABLE_ID
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 # Funcion que obtiene los links de descarga de fireload
 # el parametro folder_url es la url de la carpeta de fireload
@@ -27,3 +32,95 @@ def get_fireload_urls(folder_url):
         url = link['href']
         urls.append(url)
     return urls
+
+# Funcion que descarga los archivos de fireload
+# el parametro driver es el driver de selenium
+def download_fireload_urls(driver, urls, folder_destiny):
+
+    items_in_process = []
+    items_to_remove = []
+    items_ending = []
+
+    def wait_seconds(seconds=1):
+        sleep(seconds)
+        for item in items_in_process:
+            item['seconds'] += seconds
+
+    def window_open(driver, url):
+        driver.execute_script(f"window.open('{url}');")
+
+    def get_download_url(driver):
+        try:
+            download_link = WebDriverWait(driver, 5).until(
+                EC.visibility_of_element_located((By.ID, DOWNLOAD_LINK_ID)))
+            return download_link.get_attribute('href')
+        except:
+            print('No se pudo obtener el link de descarga')
+            return False
+
+    def exist_file_downloaded(filename):
+        return exists_file(folder_destiny, filename)
+
+    while urls or items_in_process:
+        # add news item to process
+        if len(items_in_process) < MAX_THREADS:
+            for _ in range(MAX_THREADS - len(items_in_process)):
+                if len(urls) == 0:
+                    break
+                url = urls.pop()
+                items_in_process.append({
+                    'url': url,
+                    'window_handle': None,
+                    'seconds': 0,
+                    'filename': get_filename_from_url(url),
+                    'download': 'not-started',
+                    'success': None
+                })
+                window_open(driver, url)
+                print(f"Se agregó a la cola: {url}")
+        # get current active windows
+        windows = driver.window_handles[1:]
+        # search and map for new windows
+        for item in items_in_process:
+            if item['window_handle'] is None:
+                for window_handle in windows:
+                    driver.switch_to.window(window_handle)
+                    if driver.current_url == item['url']:
+                        item['window_handle'] = window_handle
+        # search and map for download button
+        for index, item in enumerate(items_in_process):
+            if item['window_handle'] is None:
+                print("No se encontró la ventana: " + item['url'])
+                continue
+            if item['download'] == 'not-started':
+                driver.switch_to.window(item['window_handle'])
+                wait_seconds(1)
+                if item['seconds'] < 5:
+                    continue
+                download_url = get_download_url(driver)
+                if download_url == 'javascript:void(0)' or download_url == False:
+                    if item['seconds'] > 10:
+                        print("No se pudo obtener el link de descarga: " + item['url'])
+                        item_deleted = items_in_process.pop(index)
+                        item_deleted['success'] = False
+                        items_ending.append(item_deleted)
+                        driver.close()
+                else:
+                    print("Iniciando descarga: " + download_url)
+                    window_open(driver, download_url)
+                    item['download_url'] = download_url
+                    item['download'] = 'started'
+            else:
+                driver.switch_to.window(item['window_handle'])
+                wait_seconds(1)
+                if exist_file_downloaded(item['filename']):
+                    print("Se descargó el archivo: " + item['filename'])
+                    item_deleted = items_in_process.pop(index)
+                    item_deleted['success'] = True
+                    items_ending.append(item_deleted)
+                    driver.close()
+    string = input("¿Desea cerrar el script? (y/n): ")
+    if string == 'y':
+        driver.quit()
+        print("El navegador se cerró")
+    print("El navegador no se cerrará")
